d96c3b2977dbf095e5cdb19f81f1d66b23b59528
3oysen <103428192+3oysen@users.noreply.github.com>
2022-09-22 13:36:07 +0200

    navigation bar and theme color toggle added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import { useTheme, createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState, useMemo } from "react";
+import { CssBaseline } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ setMode }) {
+	const theme = useTheme();
+
+	return (
+		<div>
+			<Navbar setMode={setMode} />
+		</div>
+	);
 }
 
-export default App;
+export default function ColorModeWrapper() {
+	const [mode, setMode] = useState("light");
+	const theme = useMemo(
+		() =>
+			createTheme({
+				palette: {
+					mode,
+				},
+			}),
+		[mode]
+	);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App setMode={setMode} />
+		</ThemeProvider>
+	);
+}
